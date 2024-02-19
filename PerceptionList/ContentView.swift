0e8909112141dd242b851f71fe44655793f36e01
Stefan O'Shea <soshea@deputy.com>
2024-02-19 11:18:49 +1100

    Initial Commit

---
+++ PerceptionList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PerceptionList
+//
+//  Created by Stefan O'Shea on 19/2/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PerceptionList/PerceptionListApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerceptionListApp.swift
+//  PerceptionList
+//
+//  Created by Stefan O'Shea on 19/2/2024.
+//
+
+import SwiftUI
+
+@main
+struct PerceptionListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
