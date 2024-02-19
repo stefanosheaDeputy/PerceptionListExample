bda8255b8a01754df00695105d793d3ac4948dfb
Stefan O'Shea <soshea@deputy.com>
2024-02-19 11:30:32 +1100

    Example project to show warning

--- PerceptionList/ContentView.swift
+++ PerceptionList/ContentView.swift
@@ -5,20 +5,89 @@
 //  Created by Stefan O'Shea on 19/2/2024.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+@Reducer
+public struct Parent {
+    
+    public init() {}
+    
+    @ObservableState
+    public struct State {
+        var children: IdentifiedArrayOf<Child.State>
+        
+        public init(children: IdentifiedArrayOf<Child.State>) {
+            self.children = children
         }
-        .padding()
+    }
+    
+    public enum Action {
+        case children(IdentifiedActionOf<Child>)
+    }
+    
+    public var body: some ReducerOf<Self> {
+        EmptyReducer()
+            .forEach(\.children, action: \.children) {
+                Child()
+            }
     }
 }
 
-#Preview {
-    ContentView()
+public struct ParentView: View {
+    
+    @Perception.Bindable var store: StoreOf<Parent>
+    
+    public init(store: StoreOf<Parent>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithPerceptionTracking {
+            List {
+                ForEach(
+                    store.scope(state: \.children, action: \.children)
+                ) { childStore in
+                    WithPerceptionTracking {
+                        ChildView(store: childStore)
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Reducer
+public struct Child {
+    
+    public init() {}
+    
+    @ObservableState
+    public struct State: Identifiable {
+        public var id: UUID
+        let title: String
+        
+        public init(id: UUID = UUID(), title: String) {
+            self.id = id
+            self.title = title
+        }
+    }
+    
+    public enum Action {
+        case action
+    }
+}
+
+public struct ChildView: View {
+    private let store: StoreOf<Child>
+    
+    public init(store: StoreOf<Child>) {
+        self.store = store
+    }
+    
+    public var body: some View {
+        WithPerceptionTracking {
+            Text(store.title)
+        }
+    }
 }
--- PerceptionList/PerceptionListApp.swift
+++ PerceptionList/PerceptionListApp.swift
@@ -5,13 +5,23 @@
 //  Created by Stefan O'Shea on 19/2/2024.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct PerceptionListApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let parentStore = StoreOf<Parent>(
+                initialState: Parent.State(
+                    children: [
+                        Child.State(title: "Title 1"),
+                        Child.State(title: "Title 2")
+                    ]
+                ),
+                reducer: { Parent() }
+            )
+            ParentView(store: parentStore)
         }
     }
 }
